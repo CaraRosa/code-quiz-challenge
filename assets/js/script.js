fff3ca4b20e6f9fee0b16422b8232b282b064f61
Cara R <cara.rosa.02@gmail.com>
2023-08-28 16:00:44 -0500

    edited the Time: text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // Timer starts when user pushes start button
 function startTimer(){
     var counter = 5;
@@ -6,12 +5,12 @@ function startTimer(){
       counter--;
       if (counter >= 0) {
         span = document.getElementById("count");
-        span.innerHTML = counter;
+        span.innerHTML = "Time: " + counter;
       }
     }, 1000);
   }
   function start()
   {
-      document.getElementById("count").style="color:green;";
+      document.getElementById("count");
       startTimer();
   };
