ab0e9249b06a5004cb006c976cfa0eddd64f99f1
Cara R <cara.rosa.02@gmail.com>
2023-08-28 15:37:31 -0500

    added code to make the timer start when a user pushes the start button

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+// Timer starts when user pushes start button
+function startTimer(){
+    var counter = 5;
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById("count");
+        span.innerHTML = counter;
+      }
+    }, 1000);
+  }
+  function start()
+  {
+      document.getElementById("count").style="color:green;";
+      startTimer();
+  };
