841506348443bf849ca228fb8a40becf7961f4f2
Cara R <cara.rosa.02@gmail.com>
2023-08-29 10:11:23 -0500

    added code to generate the data for questions, responses, and answer (the carousel)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@
 var questionElement = document.querySelector("#question");
 var responeElement = document.querySelector("#responses");
 
+// Generating the data/carousel
+var questions = [
+  { question: "What is a function?", responses: [ "Reusable code", "Primitive value", "None of the above" ], answer: 0 }, { question: "what is an array?", responses: [ "List of values", "Key value pairs", "None of the above" ], answer: 2 }, { question: "What is a primitive value?", responses: [ "Only a Number", "A Boolean and a Symbol", "A Number, String, Boolean, Undefined, Symbol, and BigInt" ], answer: 2 }, { question: "What is the abbreviation JSON?", responses: [ "JASON", "JavaScript notation object", "None of the above" ], answer: 1 },
+]
+
 // Timer starts when user pushes start button
 function startTimer(){
     var counter = 5;
