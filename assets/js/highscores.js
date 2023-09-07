ad30a84efb860b3a7760ac49c6fb5f8189a36f60
Cara R <cara.rosa.02@gmail.com>
2023-09-07 14:41:24 -0500

    added code remove users' initials and high scores when they click the clear high scores button

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -18,3 +18,12 @@ document.getElementById("goBackButton").addEventListener("click", function() {
     // this will take the user to the beginning of the code quiz
     window.location.href = "index.html";
 });
+
+// event listener for the Clear high scores button
+// if the user pushes the button, it clears the users' initials and scores from local storage. To reflect that change for the user, the scores list is set to an empty string to show that the initials and high scores have been cleared on the High Scores page
+document.getElementById("clearScores").addEventListener("click", function() {
+    localStorage.removeItem("userScore");
+
+    var scoresList = document.getElementById("scoresList");
+    scoresList.innerHTML = "";
+});
